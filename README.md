81837fa8bfade64fee70a4e4a88b61946693a8a9
工匠 <1664546116@qq.com>
2021-02-20 10:40:11 +0800

    创建文件及打包文件

---
+++ README.md
@@ -0,0 +1,16 @@
+
+创建
+1.mkdir webpack-demo
+2.cd webpack-
+
+
+#####npm init -y
+安装完成出现pack.json
+
+3.npm install webpack webpack-cli --save-dev
+4.出现mode_modules and package-lock.json
+
+5.创建dist文件
+
+#####  安装loadsh依赖
+npm intstall --save loadsh
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import _ from 'loadsh'
+
 function component() {
     const element = document.createElement('div')
 
+    // loadsh,现在通过一个script 引入
     element.innerHTML = _.join(['hello','webpack'],'')
 
     return element
 }
+document.body.appendChild(component())
